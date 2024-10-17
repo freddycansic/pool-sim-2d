a5cf3f63bbdbaa29b187e49871cd3f3005442390
Freddy Cansick <freddycansick@gmail.com>
2024-10-17 18:45:12 +0100

    More

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use macroquad::prelude::*;
+
+mod ball;
+mod engine;
+mod table;
+
+const SCALE: f32 = 100.0;
+
+#[macroquad::main("pool-sim-2d")]
+async fn main() {
+    let mut balls = ball::get_starting_balls();
+
+    loop {
+        clear_background(WHITE);
+
+        engine::simulate(&mut balls, get_frame_time());
+        let collisions = engine::collisions(&mut balls);
+        engine::resolve(&collisions);
+
+        table::render();
+        balls.iter().for_each(ball::Ball::render);
+
+        next_frame().await
+    }
+}
